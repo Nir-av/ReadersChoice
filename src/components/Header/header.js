25e2c28326cc7215804044aff4ccaed1ba7eb8e7
Deathstroke-00 <55409357+Deathstroke-00@users.noreply.github.com>
2022-03-22 19:40:47 -0400

    header and navbar
     Shivam Sheth

---
+++ src/components/Header/header.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Input,Form,Button, FormGroup } from 'reactstrap';
+
+import { Link, withRouter } from "react-router-dom";
+import RcLogo from '../../Images/rcLogo.png';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <FormGroup row>
+            <Link to="/" className="logo"><img className="logo" src={RcLogo} height="150" alt="rcLogo" /></Link>
+            <Form className="form-inline my-2 my-lg-0">
+                <Input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <Button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</Button>
+            </Form>
+        </FormGroup>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default withRouter(Header);
+    
+
