b962a42eea57461a380c858013291edf47ddeffd
Deathstroke-00 <55409357+Deathstroke-00@users.noreply.github.com>
2022-03-28 12:08:16 -0400

    shivam
    footer changes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,6 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 import RcLogo from '../../Images/rcLogo.png';
 import axios from "axios";
 import './SignIn.css';
+import Footer from "../Footer/Footer";
 
 class SignIn extends Component 
 {
@@ -119,6 +120,7 @@ class SignIn extends Component
                             </Col>
                         </Row>
                     </div>
+                    <Footer></Footer>
             </React.Fragment>
         );
     }
