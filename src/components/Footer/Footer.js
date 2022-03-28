d990a326217eb9c3cf78ba9bffa44ce3d5235e48
Deathstroke-00 <55409357+Deathstroke-00@users.noreply.github.com>
2022-03-28 15:42:45 -0400

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,74 +1,63 @@
 import React,{Component} from "react";
-import "./style.css";
-import {withRouter } from "react-router-dom";
-
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { Label,FormGroup,Col,Input,Button,Form,Row } from "reactstrap";
+import { withRouter } from "react-router-dom";
 class Footer extends Component 
 {
     render() {
-        return (
-            <div className='Box'>
-            <h1 style={{ color: "green",
-                        textAlign: "center",
-                        marginTop: "-50px" }}>
-                GeeksforGeeks: A Computer Science Portal for Geeks
-            </h1>
-            <div className="Container">
-                <div className="Row">
-                <div className="Column">
-                    <p className="Heading">About Us</p>
-                    <a className="Footerlink" href="#">Aim</a >
-                    <a className="Footerlink" href="#">Vision</a >
-                    <a className="Footerlink" href="#">Testimonials</a >
-                </div>
-                <div className="Column">
-                    <p className="Heading">Services</p>
-                    <a className="Footerlink" href="#">Writing</a >
-                    <a className="Footerlink" href="#">Internships</a >
-                    <a className="Footerlink" href="#">Coding</a >
-                    <a className="Footerlink" href="#">Teaching</a >
-                </div>
-                <div className="Column">
-                    <p className="Heading">Contact Us</p>
-                    <a className="Footerlink"  href="#">Uttar Pradesh</a >
-                    <a className="Footerlink" href="#">Ahemdabad</a >
-                    <a className="Footerlink" href="#">Indore</a >
-                    <a className="Footerlink" href="#">Mumbai</a >
-                </div>
-                <div className="Column">
-                    <p className="Heading">Social Media</p>
-                    <a className="Footerlink" href="#">
-                    <i className="fab fa-facebook-f">
-                        <span style={{ marginLeft: "10px" }}>
-                        Facebook
-                        </span>
-                    </i>
-                    </a>
-                    <a className="Footerlink" href="#">
-                    <i className="fab fa-instagram">
-                        <span style={{ marginLeft: "10px" }}>
-                        Instagram
-                        </span>
-                    </i>
-                    </a>
-                    <a className="Footerlink" href="#">
-                    <i className="fab fa-twitter">
-                        <span style={{ marginLeft: "10px" }}>
-                        Twitter
-                        </span>
-                    </i>
-                    </a>
-                    <a className="Footerlink" href="#">
-                    <i className="fab fa-youtube">
-                        <span style={{ marginLeft: "10px" }}>
-                        Youtube
-                        </span>
-                    </i>
-                    </a>
-                </div>
-                </div>
-            </div>
-            </div>
-        );
+  return (
+    <MDBFooter color="#F5C6AA" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6" style={{background:"#F5C6AA",paddingLeft:"10%",color:"black",paddingTop:"1%"}}>
+            <h5 className="title">Stay Connected</h5>
+            <p style={{background:"#F5C6AA"}}>
+                <Form style={{background:"#F5C6AA"}}>
+                    <FormGroup row style={{background:"#F5C6AA"}}>
+                            <Label for="exampleEmail" sm={1}>
+                            Email
+                            </Label>
+                            <Col sm={10} style={{background:"#F5C6AA"}}><Input id="exampleEmail" name="email" placeholder="Email" type="email"/></Col> 
+                            <Row style={{background:"#F5C6AA"}}><Button>Subscribe!</Button></Row>
+                                 
+                    </FormGroup>
+                </Form>
+            </p>
+          </MDBCol>
+          
+          <MDBCol md="6" style={{background:"#F5C6AA",paddingTop:"2%",paddingLeft:"15%",color:"black"}}>
+            <h5 className="title">Quick Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!" style={{color:"black"}}>Link 1</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!" style={{color:"black"}}>Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!" style={{color:"black"}}>Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!" style={{color:"black"}}>Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow style={{background:"#F5C6AA", paddingLeft:"10%",color:"black", paddingBottom:"1%"}}>
+            <h5 className="title">Social Media</h5>
+            <a href="#!" style={{color:"black"}}>Link 2</a>
+            <a href="#!" style={{color:"black"}}>Link 2</a>
+            <a href="#!" style={{color:"black"}}>Link 2</a>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3" style={{background:"#BED7D1"}} >
+        <MDBContainer fluid style={{background:"#BED7D1",color:"black"}}>
+          &copy; {new Date().getFullYear()} Copyright: <a href="#" style={{color:"black"}}> ReadersChoice.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
     }
-};
+}
+
 export default withRouter(Footer);
