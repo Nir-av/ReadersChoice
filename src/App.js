9dc4e420ea897b6d163382a4d75e7254d177b5b8
Deathstroke-00 <55409357+Deathstroke-00@users.noreply.github.com>
2022-03-28 19:52:22 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { withRouter, Route, Switch, BrowserRouter,} from 'react-router-dom';
 import routes from './routes';
 import './App.css';
+import NonFiction from './components/Nonfiction/NonFiction';
 import Fiction from './components/Fiction/Fiction';
+import Children from './components/children/Children';
 
 class App extends Component {
 
@@ -15,6 +17,8 @@ class App extends Component {
     return(
       <React.Fragment>
         <Fiction></Fiction>
+        <NonFiction></NonFiction>
+        <Children></Children>
       </React.Fragment>
     );
   }
